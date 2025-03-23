3a31084ba96923ce29818c5feb6ebff562dc92af
brandon-alexis <brandonmachado159@gmail.com>
2025-03-23 10:05:55 -0500

    agregando clase para el manejo e textos en la interface

---
+++ src/ui/text.py
@@ -0,0 +1,31 @@
+import pygame
+from settings import *
+
+class Text:
+    def __init__(self, x, y, font_name, text, size, color):
+        self.x = x
+        self.y = y
+        self.font_name = font_name
+        self.text = text
+        self.size = size
+        self.color = color
+        self.font = None
+        self.surface = None
+        self.setup()
+        
+    def setup(self):
+        if self.font == None:
+            if self.font_name == FONT_NAME_DOTO:
+                self.font = pygame.font.Font(FONT_PATH_DOTO, self.size)
+            if self.font_name == FONT_NAME_ROBOTO:
+                self.font = pygame.font.Font(FONT_PATH_ROBOTO, self.size)
+
+            if self.surface == None:    
+                self.surface = self.font.render(self.text, True, self.color)
+                
+    def draw(self, container: pygame.Surface):
+        container.blit(self.surface, (self.x, self.y - self.surface.get_height() / 2))
+    
+    def set_text(self, text):
+        self.text = text
+        
