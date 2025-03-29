79cf4678a619b46bcabd52a764015af5df65a599
brandon-alexis <brandonmachado159@gmail.com>
2025-03-29 11:42:46 -0500

    agregando boton para reiniciar el juego

---
+++ src/ui/top/reset_button.py
@@ -0,0 +1,30 @@
+import pygame
+from settings import *
+
+class ResetButton:
+    def __init__(self, x, y, w, h):
+        self.surface = pygame.Surface((w, h))
+        self.rect = self.surface.get_rect() 
+        self.rect.topright = (x, y)
+        self.color = BUTTON_COLOR_BACKGROUND 
+        self.font = BUTTON_FONT
+        self.text_color = BUTTON_COLOR_TEXT
+        self.text = "R"
+    
+    def draw(self, container: pygame.Surface):
+       pygame.draw.rect(container, self.color, self.rect) 
+       
+       text = self.font.render(self.text, True, self.text_color)  
+       container.blit(text, (self.rect.centerx - text.get_width() // 2, self.rect.centery - text.get_height() // 2))
+    
+    def click(self):
+        return True
+    
+            
+    def hover(self):
+        self.color = BUTTON_COLOR_BACKGROUND_HOVER
+        self.text_color = BUTTON_COLOR_TEXT_HOVER
+    
+    def not_hover(self):
+        self.color = BUTTON_COLOR_BACKGROUND
+        self.text_color = BUTTON_COLOR_TEXT
