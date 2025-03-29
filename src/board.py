378651c3a2561b057b4249f4fdd96db1b13bfd60
brandon-alexis <brandonmachado159@gmail.com>
2025-03-29 11:40:00 -0500

    agregando metodo para reiniciar todos los valores del juego

--- src/board.py
+++ src/board.py
@@ -43,23 +43,20 @@ class Board:
             
     def change_turn(self):
         self.turn = CELL_SHAPE_O if self.turn == CELL_SHAPE_X else CELL_SHAPE_X
+        print(self.turn)
         
     def check_winner(self, shape):
         for row in range(self.size):
-            # Check rows
             if self.board[row][0].shape == shape and self.board[row][1].shape == shape and self.board[row][2].shape == shape:
                 self.winner = shape    
             
             for col in range(self.size):
-                # Check columns
                 if self.board[0][col].shape == shape and self.board[1][col].shape == shape and self.board[2][col].shape == shape:
                     self.winner = shape
                     
-                # Check diagonal 1
                 if self.board[0][0].shape == shape and self.board[1][1].shape == shape and self.board[2][2].shape == shape:
                     self.winner = shape
                 
-                # Check diagonal 2
                 if self.board[0][2].shape == shape and self.board[1][1].shape == shape and self.board[2][0].shape == shape:
                     self.winner = shape
                     
@@ -90,6 +87,10 @@ class Board:
         self.winner = None
         self.setup()
 
+    def reset_all(self):
+        self.turn = CELL_SHAPE_X
+        self.winner = None
+        self.setup()
                 
                 
                 
